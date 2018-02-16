a28641f41426227943bb39856f6d3f51c0be7f84
62442katieb <kbott006@fiu.edu>
2018-02-16 17:30:55 -0500

    Add functionality to CreateCollection
    Unfortunately, “add inclusion/exclusion criteria” buttons are broken?

--- brainspell/src/components/CreateCollection.vue
+++ brainspell/src/components/CreateCollection.vue
@@ -21,7 +21,7 @@
       <tab-content title="Inclusion Criteria"
                    icon="ti-thumb-up">
         <p>Enter your search string(s) here:
-          <b-form-input v-model="searchStr"
+          <b-form-input v-model="searchString"
                   type="text"
                   placeholder="Separate strings with semicolons"></b-form-input>
         </p>
@@ -36,20 +36,43 @@
               <button type="button" class="close" aria-label="Close" style="width:100%" @click="removeInc(row)">
                 <span aria-hidden="true">&times;</span>
               </button>
-
             </template>
-
           </b-table>
           <b-button size="sm" variant="outline-secondary" @click="addInclusion">Add inclusion criterion</b-button>
         </p>
       </tab-content>
       <tab-content title="Exclusion Criteria"
                    icon="ti-thumb-down">
-        What are your criteria for study exclusion?
+        <p>What are your criteria for study exclusion?
+          <b-table striped hover :items="excCriteria" :fields="excFields" ref="excTable" small>
+
+            <template slot="Exclusion" slot-scope="data">
+              <textfield v-model="data.value" :index="data.index" v-on:input="setExclusion" ttype="text"></textfield>
+            </template>
+            <template slot="delete" scope="row">
+
+              <button type="button" class="close" aria-label="Close" style="width:100%" @click="removeExc(row)">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </template>
+          </b-table>
+          <b-button size="sm" variant="outline-secondary" @click="addExclusion">Add exclusion criterion</b-button>
+        </p>
       </tab-content>
       <tab-content title="Tags"
                    icon="ti-tag">
-        Select any/all relevant tags for you collection:
+        <p>Select any/all relevant tags for you collection:
+          <b-badge v-for="desc in descriptors" class="mr-1">{{desc}}</b-badge>
+          <!-- the modal -->
+          <b-modal size="lg" ref="descriptorsModal">
+            <div slot="modal-header" class="w-100" style="display: inline">
+              <b-input class="w-100" placeholder="Search Descriptors" v-model="tagSearch"></b-input>
+            </div>
+            <descriptors :selected="descriptors" v-on:setselected="setSelector" :filter="tagSearch"></descriptors>
+          </b-modal>
+          <br>
+          <b-btn size="sm" variant="outline-secondary" @click="showDescriptors">Open Descriptors</b-btn>
+        </p>
       </tab-content>
     </form-wizard>
   </b-container>
@@ -61,7 +84,7 @@
 </style>
 
 <script>
-
+import Descriptors from './Descriptors';
 import Vue from 'vue';
 import VueFormWizard from 'vue-form-wizard'
 import 'vue-form-wizard/dist/vue-form-wizard.min.css'
@@ -101,31 +124,75 @@ export default {
       tagSearch: '',
       name: '',
       description: '',
+      searchString: '',
+      excFields: ['Criteria', 'delete'],
+      excCriteria: '',
     };
   },
   components: {
     Textfield,
+    Descriptors,
   },
   computed: {
 
   },
   methods: {
+    name() {
+      return this.name;
+    },
+    description() {
+      return this.description;
+    },
+    searchString() {
+      return this.searchString;
+    },
     incCriteria() {
-      return this.col.incCriteria;
+      return this.incCriteria;
     },
     removeInc(row) {
-      this.col.incCriteria.splice(row.index, 1);
+      this.incCriteria.splice(row.index, 1);
       this.$refs.incTable.refresh();
     },
     setInclusion(val, idx) {
-      this.col.incCriteria[idx].key = val;
+      this.incCriteria[idx] = val;
     },
     addInclusion() {
-      this.col.incCriteria.push({
+      this.incCriteria.push({
         key: '',
-        value: '',
       });
       this.$refs.incTable.refresh();
+    },
+    excCriteria() {
+      return this.excCriteria;
+    },
+    removeExc(row) {
+      this.excCriteria.splice(row.index, 1);
+      this.$refs.excTable.refresh();
+    },
+    setExclusion(val, idx) {
+      this.excCriteria[idx] = val;
+    },
+    addExclusion() {
+      this.excCriteria.push({
+        key: '',
+      });
+      this.$refs.incTable.refresh();
+    },
+    showDescriptors() {
+      this.$refs.descriptorsModal.show();
+    },
+    setSelector(item) {
+      console.log('recieved', item);
+      const idx = this.exp.descriptors.indexOf(item.name);
+      console.log('idx is', idx);
+      if (idx >= 0) {
+        // remove the descriptors
+        this.exp.descriptors.splice(idx, 1);
+      } else {
+        this.exp.descriptors.push(item.name);
+      }
+      console.log(this.exp.descriptors);
+      this.$forceUpdate();
     },
   },
   props: ['col', 'index'],
