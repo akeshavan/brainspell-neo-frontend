bfbfb865363762cdaafd7e595cf57712aec821ac
akeshavan <keshavan@berkeley.edu>
2018-04-23 16:49:40 -0700

    fix: fixed config for second oauth route

--- brainspell/src/config.js
+++ brainspell/src/config.js
@@ -1,8 +1,8 @@
 export default {
-  authUrl: 'https://brainspell.herokuapp.com/json/github-oauth',
-  clientId: '11d54ed9cbcaa3a88e15',
-  redirectUri: 'http://localhost:8080',
-  prodClientId: 'bd0ee431789e9daa276c',
+  authUrl: 'https://brainspell.herokuapp.com/json/localhost-oauth',
+  prodClientId: '11d54ed9cbcaa3a88e15',
+  redirectUri: 'localhost:8080',
+  clientId: 'bd0ee431789e9daa276c',
   prodRedirectUrl: 'http://akeshavan.github.io/brainspell-neo-frontend',
   prodAuthUrl: 'https://brainspell.herokuapp.com/json/github-oauth',
 };
