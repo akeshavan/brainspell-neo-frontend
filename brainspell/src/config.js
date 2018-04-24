a0409f440f2251156b6657d5074b98d3434d7d79
akeshavan <keshavan@berkeley.edu>
2018-04-23 17:00:52 -0700

    fix: removed the http part of the redirect in config

--- brainspell/src/config.js
+++ brainspell/src/config.js
@@ -3,6 +3,6 @@ export default {
   prodClientId: '11d54ed9cbcaa3a88e15',
   redirectUri: 'localhost:8080',
   clientId: 'bd0ee431789e9daa276c',
-  prodRedirectUrl: 'http://akeshavan.github.io/brainspell-neo-frontend',
+  prodRedirectUrl: 'akeshavan.github.io/brainspell-neo-frontend',
   prodAuthUrl: 'https://brainspell.herokuapp.com/json/github-oauth',
 };
