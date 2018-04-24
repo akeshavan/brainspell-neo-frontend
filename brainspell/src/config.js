82bac2f08f5bf84225949cd0b1cf096998461f7e
akeshavan <keshavan@berkeley.edu>
2018-04-24 09:33:16 -0700

    fix: new oauth routes

--- brainspell/src/config.js
+++ brainspell/src/config.js
@@ -1,8 +1,8 @@
 export default {
-  authUrl: 'https://brainspell.herokuapp.com/json/localhost-oauth',
+  authUrl: 'https://brainspell.herokuapp.com/json/github-oauth-development',
   prodClientId: '11d54ed9cbcaa3a88e15',
   redirectUri: 'localhost:8080',
   clientId: 'bd0ee431789e9daa276c',
   prodRedirectUrl: 'akeshavan.github.io/brainspell-neo-frontend',
-  prodAuthUrl: 'https://brainspell.herokuapp.com/json/github-oauth',
+  prodAuthUrl: 'https://brainspell.herokuapp.com/json/github-oauth-production',
 };
