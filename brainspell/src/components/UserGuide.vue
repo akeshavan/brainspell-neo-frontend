0bf29dd27a8389d74144e3b870cab29afee8525f
62442katieb <kbott006@fiu.edu>
2018-05-29 10:41:21 -0400

    Minor changes to wording in About & UserGuide
    specific language about meta-analyses added to About & small phrase
    added to UserGuide

--- brainspell/src/components/UserGuide.vue
+++ brainspell/src/components/UserGuide.vue
@@ -9,7 +9,7 @@
                 This is a guide to getting started with MetaCurious. To contribute to development, check out our <a href="#/developerguide">Developer Guide</a>.
             </p>
             <p>
-                Before you start you'll need to set up a free <a href="https://github.com/" target="_blank">GitHub</a> account and sign in.
+                Before you start you'll need to set up a free <a href="https://github.com/" target="_blank">GitHub</a> account, if you don't already have one, and sign in.
                 Here are some <a href="https://help.github.com/articles/signing-up-for-a-new-github-account" target="_blank">instructions</a>.
             </p>
             <p>
