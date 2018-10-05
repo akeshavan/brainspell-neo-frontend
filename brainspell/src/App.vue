d9b029645f50f626a4d9d4d820d5b5d21a8eb7d3
62442katieb <kbott006@fiu.edu>
2018-10-05 11:51:37 -0400

    added footer bar
    it's not cute, though, and it's especially not cute on mobile...

--- brainspell/src/App.vue
+++ brainspell/src/App.vue
@@ -88,6 +88,25 @@
 
       </b-collapse>
     </b-navbar>
+    <b-navbar toggleable="md" type="dark" variant="info" fixed="bottom">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-text>metaCurious is built on Roberto Toro's </b-navbar-text>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <b-nav-item href="https://brainspell.herokuapp.com/">Brainspell.</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">CC BY-SA 3.0</b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
 
     <!-- The content is in the router view -->
 
